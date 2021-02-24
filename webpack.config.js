59f5dee157a2f28664fec376b7eb4ab34db7b787
Yosef Blandin <yosefleanb@gmail.com>
2021-02-23 21:06:50 -0400

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ],
     },
     output: {
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "./public"),
         filename: 'bundle.js',
     },
     plugins: [
